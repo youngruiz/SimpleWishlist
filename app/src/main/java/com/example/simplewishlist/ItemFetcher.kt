ce612db3484c0793f85cf445e75f070209133e3c
youngruiz <rlod@protonmail.com>
2022-09-23 13:24:23 -0700

    Add item layout/views added

--- app/src/main/java/com/example/simplewishlist/ItemFetcher.kt
+++ app/src/main/java/com/example/simplewishlist/ItemFetcher.kt
@@ -2,12 +2,12 @@ package com.example.simplewishlist
 
 class ItemFetcher {
     companion object {
-        val itemName = listOf("Mouth Wash", "Pencils", "Soap")
-        val itemUrl = listOf("amazon.com", "target.com", "walmart.com")
-        val itemPrice = listOf("6.99", "3.99", "4.99")
+        val itemName = listOf("Mouth Wash", "Pencils", "Soap", "Notebook", "Eraser", "Lamp", "Batteries", "shoes")
+        val itemUrl = listOf("amazon.com", "target.com", "walmart.com", "officemax.com", "amazon.com", "amazon.com", "walmart.com", "stockx.com")
+        val itemPrice = listOf("6.99", "3.99", "4.99", "0.99", "0.99", "14.99", "7.99", "99.99")
         fun getItems(): MutableList<Item> {
             var items : MutableList<Item> = ArrayList()
-            for (i in 0..2) {
+            for (i in 0..itemName.size-1) {
                 val item = Item(itemName[i], itemUrl[i], itemPrice[i])
                 items.add(item)
             }
