ed1c6279b6c111fe85541a489038ef89d5389e61
youngruiz <rlod@protonmail.com>
2022-09-23 14:39:18 -0700

    Add EditText input capture

--- app/src/main/java/com/example/simplewishlist/MainActivity.kt
+++ app/src/main/java/com/example/simplewishlist/MainActivity.kt
@@ -2,8 +2,16 @@ package com.example.simplewishlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import android.app.Activity
+import android.graphics.Color
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +33,49 @@ class MainActivity : AppCompatActivity() {
         // Set layout manager to position the items
         itemsRv.layoutManager = LinearLayoutManager(this)
 
+        val submitButton = findViewById<Button>(R.id.submitButtonBt)
+        submitButton.setOnClickListener {
+
+            // Initialize variables.
+            var newItemName: String
+            var newItemNameInput: TextView
+            var newItemUrl: String
+            var newItemUrlInput: TextView
+            var newItemPrice: String
+            var newItemPriceInput: TextView
+
+            // Get input from EditText(s) and assign to variables.
+            newItemNameInput = findViewById(R.id.itemNameEt)
+            newItemName = newItemNameInput.text.toString()
+            newItemUrlInput = findViewById(R.id.itemUrlEt)
+            newItemUrl = newItemUrlInput.text.toString()
+            newItemPriceInput = findViewById(R.id.itemPriceEt)
+            newItemPrice = newItemPriceInput.text.toString()
+
+            Log.i("Daniel", newItemName)
+            Log.i("Daniel", newItemUrl)
+            Log.i("Daniel", newItemPrice)
+
+
+
+            // Reset EditText input fields
+            newItemNameInput.text = ""
+            newItemPriceInput.text = ""
+            newItemUrlInput.text = ""
+
+            // To close keyboard.
+            val view = currentFocus
+            if (view != null) {
+                view.clearFocus()
+                val inputMethodManager =
+                    getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(
+                    view.windowToken,
+                    InputMethodManager.HIDE_NOT_ALWAYS
+                )
+            }
+
+        }
 
     }
 }
